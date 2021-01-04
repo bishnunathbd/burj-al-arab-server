994a40b2eaffc50897335df6ac37a867ec9e3337
bishnunathbd <bishnunath774@gmail.com>
2021-01-04 16:57:05 +0600

    49-5-find all bookings data api

--- index.js
+++ index.js
@@ -13,13 +13,20 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://<username>:<password>@cluster0.wbtxn.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const bookings = client.db("<dbname>").collection("bookings");
+  const bookings = client.db("dbname").collection("bookings");
   
   app.post('/addBooking', (req, res) => {
     const newBooking = req.body;
     bookings.insertOne(newBooking)
     .then(result => {
       res.send(result.insertedCount > 0);
+    })
+  })
+
+  app.get('/bookings', (req, res) => {
+    bookings.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
     })
   })
 });
