25f19e7589f6fac30feb07e5f59cc00b7cbff501
bishnunathbd <bishnunath774@gmail.com>
2021-01-06 11:39:38 +0600

    49-7-verify jwt token in the serverEnd using firebase admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
 
 const app = express()
 const port = 5000
@@ -9,26 +10,54 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
+var serviceAccount = require("./configs/burj-al-arab-ph-firebase-adminsdk-xp49v-2fc3b80988.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://<username>:<password>@cluster0.wbtxn.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const bookings = client.db("dbname").collection("bookings");
-  
+  const bookings = client.db("<dbname>").collection("bookings");
+
   app.post('/addBooking', (req, res) => {
     const newBooking = req.body;
     bookings.insertOne(newBooking)
-    .then(result => {
-      res.send(result.insertedCount > 0);
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
   })
 
   app.get('/bookings', (req, res) => {
-    console.log(req.headers.authorization);
-    bookings.find({email: req.query.email})
-    .toArray((err, documents) => {
-      res.send(documents);
-    })
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const idToken = bearer.split(' ')[1]
+      console.log({ idToken });
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+          // const uid = decodedToken.uid;
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          console.log(tokenEmail, queryEmail);
+          if (tokenEmail === queryEmail) {
+            bookings.find({ email: queryEmail })
+            .toArray((err, documents) => {
+              res.send(documents);
+            })
+          }
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    }
+
+
   })
 });
 
